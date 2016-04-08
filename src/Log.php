0f11a86ff783715a2d05b493330e8be35bbdc0c9
belaustegui <cristian.alvb@gmail.com>
2016-04-08 22:55:56 +0200

    Use Drupal 7 built in autoloader
    Remove XAutoload dependency and use Drupal 7 built in autoloader instead,
    closes issue #1.

--- src/Log.php
+++ src/Log.php
@@ -1,42 +1,40 @@
 <?php
 
-namespace Drupal\nice_logger;
-
 class Log {
 
   public static function emergency($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_EMERGENCY);
+    self::invoke_watchdog($message, $tag, WATCHDOG_EMERGENCY);
   }
-  
+
   public static function alert($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_ALERT);
+    self::invoke_watchdog($message, $tag, WATCHDOG_ALERT);
   }
-  
+
   public static function critical($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_CRITICAL);
+    self::invoke_watchdog($message, $tag, WATCHDOG_CRITICAL);
   }
-  
+
   public static function error($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_ERROR);
+    self::invoke_watchdog($message, $tag, WATCHDOG_ERROR);
   }
-  
+
   public static function warning($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_WARNING);
+    self::invoke_watchdog($message, $tag, WATCHDOG_WARNING);
   }
-  
+
   public static function notice($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_NOTICE);
+    self::invoke_watchdog($message, $tag, WATCHDOG_NOTICE);
   }
-  
+
   public static function info($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_INFO);
+    self::invoke_watchdog($message, $tag, WATCHDOG_INFO);
   }
-  
+
   public static function debug($message, $tag = "") {
-    self::log($message, $tag, WATCHDOG_DEBUG);
+    self::invoke_watchdog($message, $tag, WATCHDOG_DEBUG);
   }
-  
-  private static function log($message, $tag, $severity) {
+
+  private static function invoke_watchdog($message, $tag, $severity) {
     watchdog($tag, $message, [], $severity);
   }
 }
